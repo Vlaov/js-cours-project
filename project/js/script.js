848f0f4298826771812305940712ffb6a697c1e2
prizrak4416 <hauv@mail.ru>
2021-06-24 11:41:23 +0300

    just commit

--- project/js/script.js
+++ project/js/script.js
@@ -178,12 +178,13 @@ window.addEventListener('scroll', showModalByScroll);
 //  * Используем классы для карточек
 
 class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.classes = classes;
         this.parent = document.querySelector(parentSelector); //получаем родителя который передаем при создании экземпляра класса для того чтобы в этого родителя добавить созданный нами элемент
         this.transfer = 27;
         this.changeToUAH();
@@ -195,10 +196,15 @@ class MenuCard {
 
     render() {
         const element = document.createElement('div');
+        if(this.classes.length === 0) {
+            this.element = 'menu__item';
+            element.classList.add(this.element );
+        } else {
+            this.classes.forEach(className => element.classList.add(className));
+        }
         
         /* вставляем верстку из HTML документа и подставляем туда переданные при создании экземпляра класса атрибуты */
         element.innerHTML = `
-        <div class="menu__item">
             <img src=${this.src} alt=${this.alt}>
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">${this.descr}</div>
@@ -207,7 +213,6 @@ class MenuCard {
                 <div class="menu__item-cost">Цена:</div>
                 <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
             </div>
-        </div>
     `;
     this.parent.append(element); //добавляем созданный нами элемент в родителя
     }
@@ -220,7 +225,8 @@ new MenuCard(
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    ".menu__field .container"
+    ".menu .container",
+   
 ).render();
 
 new MenuCard(
@@ -229,7 +235,9 @@ new MenuCard(
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     14,
-    ".menu__field .container" // точно указываем в какого родителя добавлять. Есть блок menu, а в нем container 
+    ".menu .container", // точно указываем в какого родителя добавлять. Есть блок menu, а в нем container 
+    "menu__item",
+    "big"
 ).render();
 
 new MenuCard(
@@ -238,7 +246,9 @@ new MenuCard(
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     21,
-    ".menu__field .container"
+    ".menu .container",
+    "menu__item",
+    "big"
 ).render();
 
         
