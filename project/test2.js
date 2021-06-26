80086eb6a46e1f28c910d50d0787d5337b86c063
prizrak4416 <hauv@mail.ru>
2021-06-26 23:09:48 +0300

    test 2 commit

---
+++ project/test2.js
@@ -0,0 +1,13 @@
+function updateLight(current) {
+    var firstColor = 'green';
+if (current = 'red') {
+    return 'green'; 
+} else if (current = 'green') {
+    return 'yellow';
+} else if (current = 'yellow') {
+    return '' + firstColor  ;
+} 
+    
+
+  }
+console.log(updateLight("yellow"));
